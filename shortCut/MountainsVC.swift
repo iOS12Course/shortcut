9cd77e5f219cb72abe8d0f52e0c8fe0ef83de6ec
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-03-28 20:19:44 -0500

    Setting up the Ui

---
+++ shortCut/MountainsVC.swift
@@ -0,0 +1,20 @@
+//
+//  MountainsVC.swift
+//  shortCut
+//
+//  Created by Ricardo Herrera Petit on 3/28/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class MountainsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ shortCut/OceanVC.swift
@@ -0,0 +1,30 @@
+//
+//  OceanVC.swift
+//  shortCut
+//
+//  Created by Ricardo Herrera Petit on 3/28/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class OceanVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
